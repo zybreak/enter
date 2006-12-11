ae1302bae7fd78660154e08dc47e0330a144cf26
Robert Knutsson <zybreak@gmail.com>
2006-12-11 20:45:22 +0000

    auth.c fixed

--- src/auth.c
+++ src/auth.c
@@ -17,6 +17,51 @@ static struct auth_t {
 	struct passwd *pwd;
 } auth;
 
+static void auth_spawn(void)
+{
+	/* If theres no shell accociated with the user in
+	 * /etc/passwd, assign the user a shell from /etc/shells.  */
+	if (!*auth.pwd->pw_shell) {
+		setusershell();
+		auth.pwd->pw_shell = getusershell();
+		endusershell();
+	}
+
+	/* Read the group database /etc/group.  */
+	if (initgroups(auth.pwd->pw_name, auth.pwd->pw_gid) == -1)
+		return;
+
+	if (setgid(auth.pwd->pw_gid) == -1) {
+		return;
+	}
+
+	if (setuid(auth.pwd->pw_uid) == -1) {
+		return;
+	}
+
+	chdir(auth.pwd->pw_dir);
+
+	char *args[] = {
+		auth.pwd->pw_shell,
+		"-login",
+		xstrcat(auth.pwd->pw_dir,"/.xinitrc"),
+		NULL
+	};
+
+	char *env[] = {
+		xstrcat("HOME=", auth.pwd->pw_dir),
+		xstrcat("SHELL=", auth.pwd->pw_shell),
+		xstrcat("USER=", auth.pwd->pw_name),
+		xstrcat("LOGNAME=", auth.pwd->pw_name),
+		xstrcat("XAUTHORITY=",
+				xstrcat(auth.pwd->pw_dir,"/.Xauthority")),
+		xstrcat("DISPLAY=", auth.display),
+		NULL
+	};
+
+	execve(args[0],args,env);
+}
+
 int auth_authenticate(cfg_t *conf, const char *username, const char *password)
 {
 	auth.pwd = getpwnam(username);
@@ -75,50 +120,5 @@ int auth_login(void)
 	}
 
 	return TRUE;
-}
-
-static void auth_spawn(void)
-{
-	/* If theres no shell accociated with the user in
-	 * /etc/passwd, assign the user a shell from /etc/shells.  */
-	if (!*auth.pwd->pw_shell) {
-		setusershell();
-		auth.pwd->pw_shell = getusershell();
-		endusershell();
-	}
-
-	/* Read the group database /etc/group.  */
-	if (initgroups(auth.pwd->pw_name, auth.pwd->pw_gid) == -1)
-		return;
-
-	if (setgid(auth.pwd->pw_gid) == -1) {
-		return;
-	}
-
-	if (setuid(auth.pwd->pw_uid) == -1) {
-		return;
-	}
-
-	chdir(auth.pwd->pw_dir);
-
-	char *args[] = {
-		auth.pwd->pw_shell,
-		"-login",
-		xstrcat(auth.pwd->pw_dir,"/.xinitrc"),
-		NULL
-	};
-
-	char *env[] = {
-		xstrcat("HOME=", auth.pwd->pw_dir),
-		xstrcat("SHELL=", auth.pwd->pw_shell),
-		xstrcat("USER=", auth.pwd->pw_name),
-		xstrcat("LOGNAME=", auth.pwd->pw_name),
-		xstrcat("XAUTHORITY=",
-				xstrcat(auth.pwd->pw_dir,"/.Xauthority")),
-		xstrcat("DISPLAY=", auth.display),
-		NULL
-	};
-
-	execve(args[0],args,env);
 }
 
