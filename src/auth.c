ca60e5970e4501175dcfe582c1304d19d06a6d21
Robert Knutsson <zybreak@gmail.com>
2006-12-23 18:48:20 +0000

    fixed warnings in auth.c finished writing documentation.

--- src/auth.c
+++ src/auth.c
@@ -3,6 +3,7 @@
 #include <shadow.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <crypt.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
--- src/enter.c
+++ src/enter.c
@@ -25,13 +25,17 @@ static void parse_args(int argc, char **argv, cfg_t *conf)
 	for (i=1;i<argc;i++) {
 		if ((strcmp(argv[i],"-c") == 0) && (i+1 < argc)) {
 			conf_set(conf,"config_file",argv[++i]);
+
 		} else if ((strcmp(argv[i],"-d") == 0) && (i+1 < argc)) {
 			conf_set(conf,"display",argv[++i]);
+
 		} else if (strcmp(argv[i],"-n") == 0) {
 			conf_set(conf,"daemon","false");
+
 		} else if (strcmp(argv[i],"-v") == 0) {
 			printf("%s version %s\n",PACKAGE,VERSION);
 			exit(EXIT_SUCCESS);
+
 		} else if (strcmp(argv[i],"-h") == 0) {
 			printf(
 			"Usage: %s: [OPTIONS]\n\n"
